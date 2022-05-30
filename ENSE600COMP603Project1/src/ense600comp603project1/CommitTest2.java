18d2aa543da38391468f75352640143b1f9dc1f3
geTureChoppa <jamesapplejuice@gmail.com>
2022-05-30 21:18:54 +1200

    Commit Test 2

--- ENSE600COMP603Project1/src/ense600comp603project1/CommitTest2.java
+++ ENSE600COMP603Project1/src/ense600comp603project1/CommitTest2.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ense600comp603project1;
 
 /**
@@ -9,5 +6,7 @@ package ense600comp603project1;
  * @author Mr Me
  */
 public class CommitTest2 {
-    
+    public static void main(String[] args) {
+        //Some Random Code
+    }
 }
