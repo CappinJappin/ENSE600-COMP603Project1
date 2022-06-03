ae1baa858f2dbe7722f186e2d883a69d28a10e1a
geTureChoppa <jamesapplejuice@gmail.com>
2022-06-03 20:47:28 +1200

    Added a GUI Main Menu (no code to control yet)

--- ENSE600COMP603Project1/src/ense600comp603project1/QuizGUI.java
+++ ENSE600COMP603Project1/src/ense600comp603project1/QuizGUI.java
@@ -1,14 +1,27 @@
 package ense600comp603project1;
 
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
 /**
  *
  * @author Mr Me
  */
 public class QuizGUI {
     public static void main(String[] args) {
-//        System.out.println("Starting the Old Project1 version...");
-//        QuizConsole app = new QuizConsole();
-//        System.out.println("And done!");
-        
+        //System.out.println("Starting the Old Project1 version...");
+        //QuizConsole app = new QuizConsole();
+        //System.out.println("And done!");
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                JFrame frame = new JFrameMenu();
+                frame.setTitle("Quiz - The Videogame");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+//                frame.setTitle("RadioButtonTest");
+//               frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//               frame.setVisible(true);
+            }
+        });
     }
 }
