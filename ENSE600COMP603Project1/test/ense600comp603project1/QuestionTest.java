5694b828a44f8c2c333ae26aad88a8948918c55e
geTureChoppa <jamesapplejuice@gmail.com>
2022-06-07 22:39:40 +1200

    added Unit Tests for QuestionTest and ViewJFrameTest

---
+++ ENSE600COMP603Project1/test/ense600comp603project1/QuestionTest.java
@@ -0,0 +1,118 @@
+package ense600comp603project1;
+
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Mr Me
+ */
+public class QuestionTest {
+    
+//    public QuestionTest() {
+//    }
+//    
+//    @BeforeClass
+//    public static void setUpClass() {
+//    }
+//    
+//    @AfterClass
+//    public static void tearDownClass() {
+//    }
+//    
+//    @Before
+//    public void setUp() {
+//    }
+//    
+//    @After
+//    public void tearDown() {
+//    }
+
+    /**
+     * Test of toString method, of class Question.
+     */
+    @Test
+    public void testToString() {
+        System.out.println("toString");
+        Question instance = null;
+        String expResult = "";
+        String result = instance.toString();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getQuestionText method, of class Question.
+     */
+    @Test
+    public void testGetQuestionText() {
+        System.out.println("getQuestionText");
+        Question instance = new Question("why?","because","because","because","because");
+        System.out.println(instance.getQuestionText());
+        System.out.println(instance.getAnswer(0));
+    }
+
+    /**
+     * Test of getRightAnswer method, of class Question.
+     */
+    @Test
+    public void testGetRightAnswer() {
+        System.out.println("getRightAnswer");
+        Question instance = null;
+        String expResult = "";
+        String result = instance.getRightAnswer();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getAnswer method, of class Question.
+     */
+    @Test
+    public void testGetAnswer() {
+        System.out.println("getAnswer");
+        int answerPosition = 0;
+        Question instance = null;
+        String expResult = "";
+        String result = instance.getAnswer(answerPosition);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getFiftyFifty method, of class Question.
+     */
+    @Test
+    public void testGetFiftyFifty() {
+        System.out.println("getFiftyFifty");
+        Question instance = null;
+        List<String> expResult = null;
+        List<String> result = instance.getFiftyFifty();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of readQuestions method, of class Question.
+     */
+    @Test
+    public void testReadQuestions() {
+        System.out.println("readQuestions");
+        String difficulty = "";
+        List<Question> expResult = null;
+        List<Question> result = Question.readQuestions(difficulty);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
---
+++ ENSE600COMP603Project1/test/ense600comp603project1/ViewJFrameTest.java
@@ -0,0 +1,42 @@
+package ense600comp603project1;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ViewJFrameTest {
+    
+    public ViewJFrameTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        EventQueue.invokeLater(() -> {
+            JFrame frame = new ViewJFrame();
+            frame.setTitle("Quiz - The Videogame");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        });
+    }
+}
